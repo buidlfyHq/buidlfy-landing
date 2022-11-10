a0faa878c1af22841053242e5b5219ae404df673
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2022-11-10 08:03:51 +0530

    Fix next config for deployment on spheron

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-console.log("isProd", isProd);
-console.log("Data", isProd ? process.env.NEXT_PUBLIC_SITE_URL : "");
 
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
-  // trailingSlash: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  trailingSlash: true,
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_SITE_URL : "",
 };
 
