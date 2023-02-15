b1cd92904c3e80a3784b124ad6f7d2291033cab0
Paarth Agarwal <56001117+paarth17032000@users.noreply.github.com>
2023-02-15 13:00:04 +0530

    Add open source section (#21)
    * Add an Opensource section and customize according to laptop and mobile

---
+++ components/Opensource/opensource-section.tsx
@@ -0,0 +1,30 @@
+import styles from "../../styles/opensource.module.scss";
+import GithubLogo from "../Icons/github";
+
+function OpensourceSection() {
+  return (
+    <div
+      className={styles.main_area}
+      data-aos="fade-up"
+      data-aos-once="true"
+      data-aos-delay="300"
+    >
+      <h5>We love OSS</h5>
+      <h1>Buidlfy is Open Source</h1>
+      <p>
+        There are lots of opportunities for designers, developers, and
+        enthusiasts to contribute to the Buidlfy ecosystem.
+      </p>
+      <button
+        className={styles.gradient_button}
+        onClick={() =>
+          window.open("https://github.com/buidlfyHq/buidlfy/", "_blank")
+        }
+      >
+        <span>Explore Code</span>
+        <GithubLogo />
+      </button>
+    </div>
+  );
+}
+export default OpensourceSection;
